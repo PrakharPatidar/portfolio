2076c2a4fc183f5b493a06ac1dbd5cd0a72e863f
prakhar_987 <prakhar.patidar@nymo.ai>
2020-04-23 00:10:18 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Header from './components/header';
+import Home from './components/home';
+import About from './components/about';
+import Work from './components/work';
+import Contact from './components/contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/portfolio'>
+            <Home />
+          </Route>
+          <Route exact path='/portfolio/about'>
+            <About />
+          </Route>
+          <Route exact path='/portfolio/work'>
+            <Work />
+          </Route>
+          <Route exact path='/portfolio/contact'>
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/work.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class work extends Component {
+    render() {
+        return (
+            <div>
+                This is my work
+            </div>
+        )
+    }
+}
+
+export default work
