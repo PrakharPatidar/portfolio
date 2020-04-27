14ab18112c946eac3d8699d2d6f580d86a86a765
prakhar_987 <prakhar.patidar@nymo.ai>
2020-04-27 19:57:48 +0530

    more css change

--- src/components/pages/work.js
+++ src/components/pages/work.js
@@ -18,6 +18,30 @@ class work extends Component {
                             real time data of various KPI along with their competitor. It helps client to compare how they are doing in market along with their competitor.<br/>
                             <b>Technologies hands on:</b> Java Script, Bootstrap, Highchart.js, IBM watson contect analytics, IBM watson explorer, Java jersey for restful API.
                         </p>
+                        <h2>
+                            Projects
+                        </h2>
+                        <h4>
+                            Marketing competitive Analysis
+                        </h4>
+                        <p>
+                            <b>Summary:</b> Developed 360 degree dashboards powered by IBM Watson Contect Analytics. Dashboards had lots of charts shows
+                            real time data of various KPI along with their competitor. It helps client to compare how they are doing in market along with their competitor.<br/>
+                            <b>Technologies hands on:</b> Java Script, Bootstrap, Highchart.js, IBM watson contect analytics, IBM watson explorer, Java jersey for restful API.
+                        </p>
+                    </div>
+                    <div className='col-md-6'>
+                        <h2>
+                            Projects
+                        </h2>
+                        <h4>
+                            Marketing competitive Analysis
+                        </h4>
+                        <p>
+                            <b>Summary:</b> Developed 360 degree dashboards powered by IBM Watson Contect Analytics. Dashboards had lots of charts shows
+                            real time data of various KPI along with their competitor. It helps client to compare how they are doing in market along with their competitor.<br/>
+                            <b>Technologies hands on:</b> Java Script, Bootstrap, Highchart.js, IBM watson contect analytics, IBM watson explorer, Java jersey for restful API.
+                        </p>
                     </div>
                 </div>
                 <div className='row'>
