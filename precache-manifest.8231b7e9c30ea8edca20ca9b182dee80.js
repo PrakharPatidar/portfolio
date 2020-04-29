b65f6f3ca8a7d85c06c792c17cf167997e3bd56c
prakhar_987 <prakhar.patidar@nymo.ai>
2020-04-30 02:46:32 +0530

    Updates

---
+++ precache-manifest.8231b7e9c30ea8edca20ca9b182dee80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302a8ff0ba519932a9ad8454b1428d98",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b647fa05303d4ed8e035",
+    "url": "/portfolio/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "05b37483dc5e45db712b",
+    "url": "/portfolio/static/css/main.20550754.chunk.css"
+  },
+  {
+    "revision": "b647fa05303d4ed8e035",
+    "url": "/portfolio/static/js/2.97e33bf8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.97e33bf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05b37483dc5e45db712b",
+    "url": "/portfolio/static/js/main.8ab400d7.chunk.js"
+  },
+  {
+    "revision": "b5c220df62d1d37f52b2",
+    "url": "/portfolio/static/js/runtime-main.789f03f7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f0d4762bf6b2fac94bf5263c148b2307.js"
+  "/portfolio/precache-manifest.8231b7e9c30ea8edca20ca9b182dee80.js"
 );
 
 self.addEventListener('message', (event) => {
